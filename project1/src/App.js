6cad227eabfe3ffdaa35601ca753a908524456f4
WalChain <95265968+Walter73BC@users.noreply.github.com>
2022-04-18 12:10:52 +0800

    Update App.js

--- project1/src/App.js
+++ project1/src/App.js
@@ -291,6 +291,8 @@ function App() {
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
+                  Project Phase : {data.projectStatus}{" "}
+                >
                   1 {CONFIG.SYMBOL} costs {CONFIG.DISPLAY_COST}{" "}
                   {CONFIG.NETWORK.SYMBOL}.
                 </s.TextTitle>
