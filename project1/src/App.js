fe5a51c9e526a22d141eca58f97042992450328a
WalChain <95265968+Walter73BC@users.noreply.github.com>
2022-05-12 17:57:41 +0800

    Update App.js

--- project1/src/App.js
+++ project1/src/App.js
@@ -98,7 +98,7 @@ function App() {
   const dispatch = useDispatch();
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
-  const projectPhase = blockchain.smartContract.methods.status().call();
+  const [projectPhase, setProjectPhase] = useState(0);
   const [claimingNft, setClaimingNft] = useState(false);
   const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
   const [mintAmount, setMintAmount] = useState(1);
@@ -127,6 +127,7 @@ function App() {
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = String(cost * mintAmount);
     let totalGasLimit = String(gasLimit * mintAmount);
+    let ProjectPhase = blockchain.smartContract.methods.status().call();
     console.log("Cost: ", totalCostWei);
     console.log("Gas limit: ", totalGasLimit);
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
