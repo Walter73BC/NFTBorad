70d5e02f6020814e429c84a39a20bef444610642
WalChain <95265968+Walter73BC@users.noreply.github.com>
2022-05-23 10:42:39 +0800

    Add files via upload

---
+++ project3/src/redux/data/dataReducer.js
@@ -0,0 +1,42 @@
+const initialState = {
+  loading: false,
+  totalSupply: 0,
+  mintPhase: -1,
+  //cost: 0,
+  //presaleCount: 0,
+  error: false,
+  errorMsg: "",
+};
+
+const dataReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "CHECK_DATA_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        error: false,
+        errorMsg: "",
+      };
+    case "CHECK_DATA_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        totalSupply: action.payload.totalSupply,
+        mintPhase: action.payload.mintPhase,
+        // cost: action.payload.cost,
+        error: false,
+        errorMsg: "",
+      };
+    case "CHECK_DATA_FAILED":
+      return {
+        ...initialState,
+        loading: false,
+        error: true,
+        errorMsg: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default dataReducer;
